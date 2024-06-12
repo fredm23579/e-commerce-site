b902578cd56cb6f972afba34af115fd4a753bb5f
fredm23579 <motta@g.ucla.edu>
2024-06-12 16:58:49 -0700

    Fix missing @vitejs/plugin-react and update build process

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -16,10 +15,10 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist', // Ensure this is set to 'dist'
+    outDir: 'dist'
   },
   test: {
     globals: true,
     environment: 'happy-dom'
   }
-})
+});
