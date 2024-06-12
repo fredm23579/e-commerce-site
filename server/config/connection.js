00d3b3e418775e363ec03ff7ec556fc7157a9161
fredm23579 <motta@g.ucla.edu>
2024-06-12 16:22:46 -0700

    updated /server/config/connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,28 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cleanDB', {
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/cleanDB';
+
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected to', connectionString);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose is disconnected');
+});
+
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  process.exit(0);
 });
 
 module.exports = mongoose.connection;
