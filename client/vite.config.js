5b662d36dd5071dc116467af8e590cae4649c907
fredm23579 <motta@g.ucla.edu>
2024-06-12 19:56:09 -0700

    Fix publish directory and ensure correct build output 9

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,26 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/graphql/, ''),
-      },
-    },
+        secure: false,
+        changeOrigin: true
+      }
+    }
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'src/assets',
+    assetsDir: 'src/assets'
   },
   test: {
     globals: true,
-    environment: 'happy-dom',
-  },
+    environment: 'happy-dom'
+  }
 });
