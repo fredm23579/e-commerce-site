e2365ea22301c408d99238479fca59a28c050345
fredm23579 <motta@g.ucla.edu>
2024-06-12 19:06:49 -0700

    Fix publish directory and ensure correct build output 5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +16,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist' 
+    outDir: 'dist',
   },
   test: {
     globals: true,
